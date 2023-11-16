19d197f45d865fd1ce0a70182ceb5da576bc9896
Zihadul Islam <zihadislam2429@gmail.com>
2023-11-16 11:35:18 +0600

    file system - sync & async

---
+++ file.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+// reading a file text
+const readText = fs.readFileSync("./texts/read.txt", "utf-8");
+
+// writing a text
+const writtenText = fs.writeFileSync(
+  "./texts/write.txt",
+  readText + "This is my written text"
+);
+
+console.log(readText);
+console.log(writtenText);
